0592918050ae06434ffc04d86dd691072cbe057c
Isha <ishac1017@gmail.com>
2025-06-04 14:46:08 -0500

    Created user interface

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            Color(.white)
+                .ignoresSafeArea()
+            VStack {
+                Text("What is your name?")
+                    .font(.title)
+                    .background(.yellow)
+                TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .border(Color.gray, width: 1)
+                    .background(.gray)
+                Button("Button"){
+                    
+                }
+                //end button
+                .font(.title)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+            }
+            //end vstack
+            .padding()
+            .background(.blue)
+        }
+        //end zstack
         
+
     }
     //end body
 }
